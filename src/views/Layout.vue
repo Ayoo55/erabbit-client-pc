61651008bb2a17dd4d234228cb8ee8763114ed50
Ayoo <Hazell55@163.com>
2022-08-05 14:43:44 +0800

    首页主体-左侧分类-结构渲染

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,8 +1,10 @@
 <template>
   <AppNavbar></AppNavbar>
   <AppHeader></AppHeader>
+  <AppHeaderSticky></AppHeaderSticky>
   <div class="app-body">
-    主体
+    <!-- 左侧分类组件 -->
+    <HomeCategory></HomeCategory>
     <router-view ></router-view>
   </div>
   <AppFooter></AppFooter>
@@ -11,13 +13,17 @@
 import AppNavbar from '../components/app-navbar.vue'
 import AppHeader from '../components/app-header.vue'
 import AppFooter from '../components/app-footer.vue'
+import AppHeaderSticky from '../components/app-header-sticky.vue'
+import HomeCategory from './home/components/home-category.vue'
 import { useStore } from 'vuex'
 export default {
   name: 'LayoutVue',
   components: {
     AppNavbar,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky,
+    HomeCategory
   },
   setup () {
     const store = useStore()

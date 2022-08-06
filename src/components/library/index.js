4d5ea7246db921a5532473ff18d1085980709224
Ayoo <Hazell55@163.com>
2022-08-06 23:33:44 +0800

    图片懒加载

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -2,10 +2,32 @@
 import XtxSkeleton from './xtx-skeleton.vue'
 import XtxCarousel from './xtx-carousel.vue'
 import XtxMore from './xtx-more.vue'
+import defaultImg from '@/assets/images/200.png'
 export default {
   install (app) {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
+    defineDirective(app)
   }
+}
+
+const defineDirective = (app) => {
+  // 图片懒加载指令
+  app.directive('lazy', {
+    mounted (el, binding) {
+      const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          observer.unobserve(el)
+          el.onerror = () => {
+            el.src = defaultImg
+          }
+          el.src = binding.value
+        }
+      }, {
+        threshold: 0.01
+      })
+      observer.observe(el)
+    }
+  })
 }

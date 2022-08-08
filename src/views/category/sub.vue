8119869c7ee23bbbf23a092533b1541905721574
Ayoo <Hazell55@163.com>
2022-08-08 16:29:17 +0800

    二级类目-筛选区展示

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -3,15 +3,19 @@
     <div class="container">
       <!-- 面包屑 -->
       <SubBread />
+      <!-- 筛选组件 -->
+      <SubFilter></SubFilter>
     </div>
   </div>
 </template>
 <script>
 import SubBread from './components/sub-bread.vue'
+import SubFilter from './sub-filter.vue'
 export default {
   name: 'SubCategory',
   components: {
-    SubBread
+    SubBread,
+    SubFilter
   }
 }
 </script>

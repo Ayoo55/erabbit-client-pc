a4f55a7ebe59718569ab3f881a9e703531c7ca6b
Ayoo <Hazell55@163.com>
2022-08-08 17:49:22 +0800

    @vueuse/core 实现双向数据绑定

--- src/components/library/xtx-checkbox.vue
+++ src/components/library/xtx-checkbox.vue
@@ -6,7 +6,8 @@
   </div>
 </template>
 <script>
-import { ref, watch } from 'vue'
+// import { ref, watch } from 'vue'
+import { useVModel } from '@vueuse/core'
 export default {
   name: 'XtxCheckbox',
   props: {
@@ -16,14 +17,27 @@ export default {
     }
   },
   setup (props, { emit }) {
-    const checked = ref(false)
+    // const checked = ref(false)
+    // const changeChecked = () => {
+    //   checked.value = !checked.value
+    //   emit('update:modelValue', checked.value)
+    // }
+    // watch(() => props.modelValue, (newValue) => {
+    //   checked.value = props.modelValue
+    // }, { immediate: true })
+    // return { checked, changeChecked }
+
+    // useVModel 方法，可以实现双向数据绑定 v-model 指令
+    // 1.props 接收默认的 modelValue
+    // 2. useVModel 来包装props 中的 modelValue 属性
+    // 3. checked.value 就是使用父组件的数据
+    // 4.如果要调用父组件的其他事件，可以使用 emit()方法，两个参数分别为事件名，与传入的值
+    const checked = useVModel(props, 'modelValue', emit)
     const changeChecked = () => {
-      checked.value = !checked.value
-      emit('update:modelValue', checked.value)
+      const newVal = !checked.value
+      checked.value = newVal
+      emit('change', newVal)
     }
-    watch(() => props.modelValue, (newValue) => {
-      checked.value = props.modelValue
-    }, { immediate: true })
     return { checked, changeChecked }
   }
 }

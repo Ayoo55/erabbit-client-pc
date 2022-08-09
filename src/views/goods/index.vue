03797616dc416ce83eb1c17c5a8693b98f52528a
Ayoo <Hazell55@163.com>
2022-08-09 16:32:14 +0800

    商品详情-图片预览组件

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class='xtx-goods-page'>
+  <div class='xtx-goods-page' v-if="goods">
     <div class="container">
       <!-- 面包屑 -->
-      <XtxBread>
-        <XtxBreadItem to="/">首页</XtxBreadItem>
-        <XtxBreadItem v-if="goods" :to="`/category/${goods.categories[1].id}`">{{goods.categories[1].name}}</XtxBreadItem>
-        <XtxBreadItem v-if="goods" :to="`/category/sub/${goods.categories[0].id}`">{{goods.categories[0].name}}</XtxBreadItem>
-        <XtxBreadItem v-if="goods" >{{goods.name}}</XtxBreadItem>
-      </XtxBread>
-      <!-- 商品信息 -->
-      <div class="goods-info"></div>
-      <!-- 商品推荐 -->
-      <GoodsRelevant v-if="goods"/>
-      <!-- 商品详情 -->
-      <div class="goods-footer">
-        <div class="goods-article">
-          <!-- 商品+评价 -->
-          <div class="goods-tabs"></div>
-          <!-- 注意事项 -->
-          <div class="goods-warn"></div>
+        <XtxBread>
+            <XtxBreadItem to="/">首页</XtxBreadItem>
+            <XtxBreadItem  :to="`/category/${goods.categories[1].id}`">{{goods.categories[1].name}}</XtxBreadItem>
+            <XtxBreadItem  :to="`/category/sub/${goods.categories[0].id}`">{{goods.categories[0].name}}</XtxBreadItem>
+            <XtxBreadItem  >{{goods.name}}</XtxBreadItem>
+        </XtxBread>
+        <!-- 商品信息 -->
+        <div class="goods-info">
+            <div class="media">
+                <GoodsImage :images="goods.mainPictures"></GoodsImage>
+            </div>
+            <div class="spec"></div>
         </div>
-        <!-- 24热榜+专题推荐 -->
-        <div class="goods-aside"></div>
-      </div>
+        <!-- 商品推荐 -->
+        <GoodsRelevant/>
+        <!-- 商品详情 -->
+        <div class="goods-footer">
+            <div class="goods-article">
+            <!-- 商品+评价 -->
+            <div class="goods-tabs"></div>
+            <!-- 注意事项 -->
+            <div class="goods-warn"></div>
+            </div>
+            <!-- 24热榜+专题推荐 -->
+            <div class="goods-aside"></div>
+        </div>
     </div>
   </div>
 </template>
@@ -32,9 +37,10 @@ import { useRoute } from 'vue-router'
 import GoodsRelevant from './components/goods-relevant'
 import { findGoods } from '@/api/product.js'
 import { nextTick, ref, watch } from 'vue'
+import GoodsImage from './components/goods-image.vue'
 export default {
   name: 'XtxGoodsPage',
-  components: { GoodsRelevant },
+  components: { GoodsRelevant, GoodsImage },
   setup () {
     const goods = useGoods()
     console.log(goods)
@@ -67,6 +73,16 @@ const useGoods = () => {
 .goods-info {
   min-height: 600px;
   background: #fff;
+  display: flex;
+  .media {
+    width: 580px;
+    height: 600px;
+    padding: 30px 50px;
+  }
+  .spec {
+    flex: 1;
+    padding: 30px 30px 30px 0;
+  }
 }
 .goods-footer {
   display: flex;

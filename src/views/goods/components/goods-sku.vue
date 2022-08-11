0a6f0309cdc468aeb52829e688369548a376c58b
Ayoo <Hazell55@163.com>
2022-08-11 11:06:15 +0800

    规格组件-禁用效果-设置状态

--- src/views/goods/components/goods-sku.vue
+++ src/views/goods/components/goods-sku.vue
@@ -4,8 +4,8 @@
       <dt>{{item.name}}</dt>
       <dd>
         <template v-for="val in item.values" :key="val.name">
-            <img :class="{selected:val.selected}" @click="changeSku(item,val)" v-if="val.picture" :src="val.picture" :title="val.name">
-            <span :class="{selected:val.selected}" @click="changeSku(item,val)"  v-else>{{val.name}}</span>
+            <img :class="{selected:val.selected,disabled:val.disabled}" @click="changeSku(item,val)" v-if="val.picture" :src="val.picture" :title="val.name">
+            <span :class="{selected:val.selected,disabled:val.disabled}" @click="changeSku(item,val)"  v-else>{{val.name}}</span>
         </template>
       </dd>
     </dl>
@@ -13,6 +13,55 @@
   </div>
 </template>
 <script>
+import getPowerSet from '@/vender/power-set'
+const spliter = '★'
+// 获取路径字典
+const getPathMap = (skus) => {
+  // 1.获取所有skus,从props传入
+  // 2.得到所有有效的sku,遍历skus
+  const pathMap = {}
+  skus.forEach(sku => {
+    if (sku.inventory) {
+      // 3.重新整理 sku 中的 有效值——valueName
+      const specs = sku.specs.map(spec => spec.valueName)
+      //   4.得到sku属性值数组的子集
+      const powerSet = getPowerSet(specs)
+      //   5.遍历子集数组，生成对象的键
+      powerSet.forEach(set => {
+        // 6.因为数组中可能有多个数据，利用join拼接成字符串，从而作为对象的键
+        const key = set.join(spliter)
+        // 如果对象中有这个键，则追加值
+        if (pathMap[key]) {
+          pathMap[key].push(sku.id)
+        } else {
+          pathMap[key] = [sku.id]
+        }
+      })
+    }
+  })
+  return pathMap
+}
+// 获取禁用状态
+const updateDisabledStatus = (specs, pathMap) => {
+  specs.forEach((item, i) => {
+    const selectedValues = getSelectedArr(specs)
+    item.values.forEach(val => {
+      if (val.selected) return
+      selectedValues[i] = val.name
+      const key = selectedValues.filter(value => value).join(spliter)
+      val.disabled = !pathMap[key]
+    })
+  })
+}
+// 获取选中按钮
+const getSelectedArr = (specs) => {
+  const arr = []
+  specs.forEach(item => {
+    const selectedVal = item.values.find(val => val.selected)
+    arr.push(selectedVal ? selectedVal.name : undefined)
+  })
+  return arr
+}
 export default {
   name: 'GoodsSku',
   props: {
@@ -21,16 +70,24 @@ export default {
       default: () => {}
     }
   },
-  setup () {
+  setup (props) {
+    const pathMap = getPathMap(props.goods.skus)
+    updateDisabledStatus(props.goods.specs, pathMap)
     const changeSku = (item, val) => {
+      if (val.disabled) {
+        return false
+      }
       if (val.selected) {
         val.selected = false
       } else {
-        item.values.forEach(v => { v.selected = false })
+        item.values.forEach(v => {
+          v.selected = false
+        })
+        val.selected = true
       }
-      val.selected = true
+      updateDisabledStatus(props.goods.specs, pathMap)
     }
-    return { changeSku }
+    return { changeSku, pathMap }
   }
 }
 </script>

9ffc7d5bceea559ab47c50ea6b91a25a1630f64e
Ayoo <Hazell55@163.com>
2022-08-18 17:38:29 +0800

    展示支付的订单相关信息

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
-import { ref } from 'vue'
-import { useIntersectionObserver } from '@vueuse/core'
+import { onUnmounted, ref } from 'vue'
+import { useIntersectionObserver, useIntervalFn } from '@vueuse/core'
+import dayjs from 'dayjs'
 export const useLazyData = (apiFn) => {
   const result = ref([])
   const target = ref(null)
@@ -18,4 +19,28 @@ export const useLazyData = (apiFn) => {
     }
   )
   return { target, result }
+}
+
+export const usePayTime = () => {
+  // 倒计时逻辑
+  const time = ref(0)
+  const timeText = ref('')
+  const { pause, resume } = useIntervalFn(() => {
+    time.value--
+    timeText.value = dayjs.unix(time.value).format('mm分ss秒')
+    if (time.value <= 0) {
+      pause()
+    }
+  }, 1000, false)
+  onUnmounted(() => {
+    pause()
+  })
+
+  // 开启倒计时
+  const start = (countdown) => {
+    time.value = countdown
+    timeText.value = dayjs.unix(time.value).format('mm分ss秒')
+    resume()
+  }
+  return { start, timeText }
 }
--- src/views/member/pay/index.vue
+++ src/views/member/pay/index.vue
@@ -11,7 +11,8 @@
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分59秒</span>, 超时后将取消订单</p>
+          <p v-if="order.countdown">支付还剩 <span>{{timeText}}</span>, 超时后将取消订单</p>
+          <p v-else>订单已经超时</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
@@ -42,16 +43,21 @@
 import { findOrderDetail } from '@/api/order'
 import { useRoute } from 'vue-router'
 import { ref } from 'vue'
+import { usePayTime } from '@/hooks'
 export default {
   name: 'XtxPayPage',
   setup () {
     const route = useRoute()
     const order = ref(null)
+    // 获取订单详情
     findOrderDetail(route.query.orderId).then(data => {
       order.value = data.result
-      console.log(order.value.payMoney)
+      if (data.result.countdown > -1) {
+        start(data.result.countdown)
+      }
     })
-    return { order }
+    const { start, timeText } = usePayTime()
+    return { order, timeText }
   }
 }
 </script>

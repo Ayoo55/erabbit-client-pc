0654da4336439c0484bd1fb8c432b3dc3a11fe01
Ayoo <Hazell55@163.com>
2022-08-23 10:12:55 +0800

    封装一个高可用tabs组件

---
+++ src/components/library/xtx-tabs-panel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-show="activeName === name" class="xtx-tabs-panel">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue'
+
+export default {
+
+  name: 'XtxTabsPanel',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  setup () {
+    const activeName = inject('activeName')
+    return { activeName }
+  }
+
+}
+</script>
--- src/views/member/order/index.vue
+++ src/views/member/order/index.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>MemberOrder
-     <router-link to="/member/order/111">订单1</router-link>
-    <router-link to="/member/order/222">订单2</router-link>
+  <div>
+    MemberOrder
+    <XtxTabs v-model="activeName">
+      <XtxTabsPanel label="选项1" name="first">内容1</XtxTabsPanel>
+      <XtxTabsPanel label="选项2" name="second">内容2</XtxTabsPanel>
+
+    </XtxTabs>
+
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
 
-  name: 'MemberOrder'
+  name: 'MemberOrder',
+  setup () {
+    const activeName = ref('second')
+    return { activeName }
+  }
 
 }
 </script>

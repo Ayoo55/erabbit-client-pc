2c097763d866798c4c225ff7df9e35f51c112b24
Ayoo <Hazell55@163.com>
2022-08-23 18:18:12 +0800

    完成订单详情头部展示

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>MemberDetail
-
+   <div class="order-detail-page">
+    <!-- 操作栏 -->
+    <OrderDetailAction />
+    <!-- 步骤条-->
+    <!-- 物流栏 -->
+    <!-- 订单商品信息 -->
   </div>
 </template>
 
 <script>
+import OrderDetailAction from './components/detail-action.vue'
 export default {
 
-  name: 'MemberOrderDetail'
+  name: 'MemberOrderDetail',
+  components: { OrderDetailAction }
 
 }
 </script>
